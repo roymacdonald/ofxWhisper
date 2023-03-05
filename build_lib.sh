7ddf2499c40ada383bada96a7122cfdfbb295027
Roy Macdonald <macdonald.roy@gmail.com>
2023-03-05 17:11:41 -0300

    Improved build_lib script

--- build_lib.sh
+++ build_lib.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+check_make_dir(){
+	if [ ! -d "$1" ]; then
+		mkdir "$1"
+	fi
+}
+
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR	
 
@@ -7,13 +14,17 @@ git clone --depth 1  https://github.com/ggerganov/whisper.cpp.git
 
 cd whisper.cpp
 
+
+check_make_dir ../libs/whisper_cpp/models/
+
 ./models/download-ggml-model.sh base.en
 
 make libwhisper.a	
 
+
 cp libwhisper.a ../libs/whisper_cpp/lib/osx/
 cp whisper.h ../libs/whisper_cpp/include/
-cp ggml.h ../libs/whisper_cpp/include/	
+cp ggml.h ../libs/whisper_cpp/include/
 cp examples/common.h ../libs/whisper_cpp/include/
 cp examples/common.cpp ../libs/whisper_cpp/src/
 cp models/ggml-base.en.bin ../libs/whisper_cpp/models/
